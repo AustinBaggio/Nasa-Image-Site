c3defd66a8393352fbe58afa4d37af9672ba5492
AustinBaggio <austinbaggio@gmail.com>
2017-12-01 18:00:18 -0500

    added server

---
+++ src/app/message.js
@@ -0,0 +1,12 @@
+var mongoose     = require('mongoose');
+var Schema       = mongoose.Schema;
+
+var MessageSchema   = new Schema({
+    timeStamp: String,
+    courseID: String,
+    messageBody: String,
+    count: String
+    
+});
+
+module.exports = mongoose.model('Message', MessageSchema);
